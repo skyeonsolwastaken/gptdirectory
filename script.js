0611a099c8b0de8a69463aad775069260a41d23f
SkyeOnSol <102369791+skyeonsolwastaken@users.noreply.github.com>
2023-11-15 14:51:14 -0600

    Update script.js
    Updated button prefix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const gptTools = [
-        { id: 1, name: 'GPT Tool A', category: 'communication', description: 'Description for Tool A', link: '#' },
+        { id: 1, name: 'Cat-GPT', category: 'communication', description: 'Have a chat with a cat!', link: 'https://chat.openai.com/g/g-Feqdu33Ug-cat-gpt' },
         { id: 2, name: 'GPT Tool B', category: 'dalle-3', description: 'Description for Tool B', link: '#' },
         { id: 3, name: 'GPT Tool C', category: 'code', description: 'Description for Tool C', link: '#' },
         // ... other tools
@@ -11,18 +11,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const categoryItems = document.querySelectorAll('.category-item');
     const menuIcon = document.querySelector('.menu-icon');
     const menuContainer = document.querySelector('.menu-container');
+    const searchInput = document.getElementById('searchInput');
+    let currentFilter = 'all';
 
-    const renderList = (filterValue) => {
+    const renderList = (filterValue, searchTerm = '') => {
         container.innerHTML = '';
         gptTools
             .filter(tool => filterValue === 'all' || tool.category === filterValue)
+            .filter(tool => tool.name.toLowerCase().includes(searchTerm.toLowerCase()))
             .forEach(tool => {
                 const itemDiv = document.createElement('div');
                 itemDiv.className = 'gpt-item';
                 itemDiv.innerHTML = `
                     <h3>${tool.name}</h3>
                     <p>${tool.description}</p>
-                    <a href="${tool.link}" target="_blank" rel="noopener noreferrer">Learn More</a>
+                    <a href="${tool.link}" target="_blank" rel="noopener noreferrer">Open GPT</a>
                 `;
                 container.appendChild(itemDiv);
             });
@@ -30,8 +33,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     categoryItems.forEach(item => {
         item.addEventListener('click', () => {
-            const category = item.getAttribute('data-category');
-            renderList(category);
+            currentFilter = item.getAttribute('data-category');
+            renderList(currentFilter, searchInput.value);
             if (window.innerWidth <= 768) {
                 menuContainer.style.width = '0';
             }
@@ -44,6 +47,10 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             menuContainer.style.width = '250px';
         }
+    });
+
+    searchInput.addEventListener('input', (e) => {
+        renderList(currentFilter, e.target.value);
     });
 
     renderList('all'); // Render all items initially
